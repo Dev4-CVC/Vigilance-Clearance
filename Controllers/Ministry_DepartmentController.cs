69a985633dd590b4826796f5e2b7c67a1dfb2907
Dev2-CVC <dev2.it-cvc@govcontractor.in>
2025-06-20 18:38:13 +0530

    add ministry module

---
+++ Controllers/Ministry_DepartmentController.cs
@@ -0,0 +1,87 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using VigilanceClearance.Models.ViewModel;
+
+namespace VigilanceClearance.Controllers
+{
+    public class Ministry_DepartmentController : Controller
+    {
+        private readonly IHttpClientFactory _clientFactory;
+
+        public Ministry_DepartmentController(IHttpClientFactory clientFactory)
+        {
+            this._clientFactory = clientFactory;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> ViewReferenceReceivedCVC()
+        {
+            try
+            {
+                // iski jagah dusri API Call Karni As per requrement
+                var client = _clientFactory.CreateClient();
+                var countries = await client.GetFromJsonAsync<List<CountryModel>>("https://restcountries.com/v3.1/all?fields=name,cca2");
+                // iski jagah dusri API Call Karni As per requrement
+            }
+            catch (Exception ex)
+            {
+                throw new Exception(ex.Message);
+            }
+            return View();
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> viewDetailsReferenceReceivedCVC()
+        {
+            try
+            {
+                // iski jagah dusri API Call Karni As per requrement
+                var client = _clientFactory.CreateClient();
+                var countries = await client.GetFromJsonAsync<List<CountryModel>>("https://restcountries.com/v3.1/all?fields=name,cca2");
+
+                // iski jagah dusri API Call Karni As per requrement
+            }
+            catch (HttpRequestException ex)
+            {
+                ModelState.AddModelError(string.Empty, "Failed to load country list.");
+                return View(new CountryViewModel { CountryList = new List<SelectListItem>() });
+
+            }
+            return View();
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> UpdateReferenceReceivedCVC()
+        {
+            try
+            {
+                // iski jagah dusri API Call Karni As per requrement
+                var client = _clientFactory.CreateClient();
+                var countries = await client.GetFromJsonAsync<List<CountryModel>>("https://restcountries.com/v3.1/all?fields=name,cca2");
+
+                var model = new UpdateReferenceReceivedFromCVCModel
+                {
+                    CountryList = countries.OrderBy(c => c.Name.Common).Select(c => new SelectListItem
+                    {
+                        Text = c.Name.Common,
+                        Value = c.Cca2
+                    }).ToList()
+                };
+
+                return View(model);
+                // iski jagah dusri API Call Karni As per requrement
+            }
+            catch (Exception ex)
+            {
+                ModelState.AddModelError(string.Empty, "Failed to load country list.");
+                return View(new CountryViewModel { CountryList = new List<SelectListItem>() });
+            }
+            //return View();
+        }
+    }
+}
---
+++ Models/ViewModel/UpdateReferenceReceivedFromCVCModel.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.AspNetCore.Mvc.Rendering;
+using System.ComponentModel.DataAnnotations;
+
+namespace VigilanceClearance.Models.ViewModel
+{
+    public class UpdateReferenceReceivedFromCVCModel
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Display(Name ="Name of the Officer")]
+        public string? Officer_Name { get; set; }
+        [Display(Name = "Father's Name")]
+        public string? Officer_FatherName { get; set; }
+        [Display(Name = "Date of Birth")]
+        public string? Officer_DateOfBirth { get; set; }
+        [Display(Name = "Date Of Retirement")]
+        public string? Officer_RetirementDate { get; set; }
+        [Display(Name = "Date of Entry into Service")]
+        public string? Officer_ServiceEntryDate { get; set; }
+        [Display(Name = "Service")]
+        public string? Officer_Service { get; set; }
+        [Display(Name = "Batch")]
+        public string? Officer_Batch_Year { get; set; }
+
+        public string? Cadre { get; set; }
+
+
+        public string? SelectedCountry { get; set; }
+
+        public List<SelectListItem>? CountryList { get; set; }
+
+//7
+        public string? Organization { get; set; }
+        public string? Designation { get; set; }
+        public DateTime TenureFrom { get; set; }
+        public DateTime TenureTo { get; set; }
+
+//8
+
+        public bool? IsAgreed { get; set; }
+        public DateTime YearFrom { get; set; }
+        public DateTime YearTo { get; set; }
+
+    }
+}
